1546377d3ae38cbfd1a4019a036b624293a5b805
joelantonyjoy <joelantonyjoy@gmail.com>
2021-01-21 15:15:33 +0530

    Changes to home CTA and Title change

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,8 @@
 export function Home(){
     return (
         <div>
-            <h1>[CompanyWebsite for feature branch]</h1>
+            <h1>Welcome to Joel Antony Joy's Portfolio. The website is currently under construction.</h1>
+            <p>Do follow me on <a rel="noopener noreferrer" target='_blank' href='https://www.linkedin.com/in/joel-antony-joy/'>LinkedIn</a> for the time being :)</p>
         </div>
     );
 }
