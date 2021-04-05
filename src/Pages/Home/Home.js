f98114c6f2c487ec41f2a82153e9f4893ef506f1
joelantonyjoy <joelantonyjoy@gmail.com>
2021-04-05 20:42:38 +0530

    Added social media handles

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,5 +1,8 @@
 import {Link} from 'react-router-dom'
-import img from './developer.jpg'
+import img from '../../assets/images/developer.jpg'
+import insta from '../../assets/images/instagram-logo.svg'
+import linkedin from '../../assets/images/linkedin-logo.svg'
+import github from '../../assets/images/github-logo.svg'
 import styles from "./Home.module.css"
 export function Home(){
     return (
@@ -29,7 +32,11 @@ export function Home(){
             <div className={styles.img__container}>
                 <img className={styles.img__content} alt='dev' src={img}></img>
             </div>
-            
+            <div style={{display:'flex',justifyContent:'space-between',width:200,alignItems:'center',marginTop:8}}>
+                <a href='https://www.instagram.com/i.joel_antony_joy' rel="noreferrer" target='_blank'><img alt='insta' src={insta}></img></a>
+                <a href='https://www.linkedin.com/in/joel-antony-joy' rel="noreferrer" target='_blank'><img alt='linkedin' src={linkedin}></img></a>
+                <a href='https://github.com/joelantonyjoy' rel="noreferrer" target='_blank'><img alt='github' src={github}></img></a>
+            </div>
         </div>
     );
 }
