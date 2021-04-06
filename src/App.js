a7a4986b94fab5cef14850fa15f81da97e8374f4
joelantonyjoy <joelantonyjoy@gmail.com>
2021-04-06 15:48:11 +0530

    Home page design changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import {Contact} from './Pages/Contact/Contact';
 import {Whoops404} from './Pages/Whoops404';
 import {Jokes} from './Pages/Projects/Jokes/Jokes';
 import Pomodoro from './Pages/Projects/Pomodoro/Pomodoro';
-
+import logo_web from './assets/images/logo-full.svg';
+import logo_mobile from './assets/images/logo-short.svg';
 import {Routes,Route,Link} from 'react-router-dom';
 
 function toggle(e){
@@ -29,20 +30,22 @@ function App() {
 return (
   <React.Fragment>
   <div className="nav-container">
+  <img className="navbar-logo" src={logo_web} alt='joel'></img>
   <nav className="navbar">
-    <Link className="link" to="/">Home</Link>
-    <Link className="link" to="projects">Projects</Link>
-    <Link className="link" to="blog">Blog</Link>
-    <Link className="link" to="contact">Contact</Link>
+    <Link className="link" to="/">HOME</Link>
+    <Link className="link" to="projects">PROJECTS</Link>
+    <Link className="link" to="blog">BLOG</Link>
+    <Link className="link" to="contact">CONTACT</Link>
   </nav>
   </div>
   <div className="nav-small">
+  <img class="navbar-logo-mob" src={logo_mobile} alt="joel"></img>
   <div className="toggle__button" onClick={toggle}>=</div>
   <nav className="navbar-small">
-    <Link onClick={closeMenu} className="link-small" to="/">Home</Link>
-    <Link onClick={closeMenu} className="link-small" to="projects">Projects</Link>
-    <Link onClick={closeMenu} className="link-small" to="blog">Blog</Link>
-    <Link onClick={closeMenu} className="link-small" to="contact">Contact</Link>
+    <Link onClick={closeMenu} className="link-small" to="/">HOME</Link>
+    <Link onClick={closeMenu} className="link-small" to="projects">PROJECTS</Link>
+    <Link onClick={closeMenu} className="link-small" to="blog">BLOG</Link>
+    <Link onClick={closeMenu} className="link-small" to="contact">CONTACT</Link>
   </nav>
   </div>
     <Routes>
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,5 +1,4 @@
 import {Link} from 'react-router-dom'
-import img from '../../assets/images/developer.jpg'
 import insta from '../../assets/images/instagram-logo.svg'
 import linkedin from '../../assets/images/linkedin-logo.svg'
 import github from '../../assets/images/github-logo.svg'
@@ -10,29 +9,26 @@ export function Home(){
             <div className={styles.profile}>
 
                 <h1 className={styles.profile__name}>Joel Antony Joy</h1>
-                <h3>Software Engineer / Full Stack Developer</h3>
+                <h3 className={styles.profile__role}>Software Engineer / Full Stack Developer</h3>
 
-                <p className={styles.profile__description}>
+                {/* <p className={styles.profile__description}>
                     I am a Software Engineer with more than 1.6 years of experience in building responsive dynamic web applications with latest web technologies. 
                     I have strong problem solving capabilities and has been awarded with an Honor Award by my current employer Nissan Digital India.
-                </p>
+                </p> */}
 
                 <Link className={styles.profile__resume}
                 to="Resume/Joel_Antony_Joy_Resume.pdf" target="_blank" download aria-label="Download Joel's Resume">
                 Download Resume
                 </Link>
-                <p style={{marginTop:'10vh'}} >
+                {/* <p style={{marginTop:'10vh'}} >
                 The website is currently under construction.
                 </p>
                 <p>Do follow me on other social media for the time being :)</p>
                 <a rel="noopener noreferrer" target='_blank' href='https://www.linkedin.com/in/joel-antony-joy/' aria-label="Joel's Linkedin Profile Link">
                   LinkedIn
-                </a>
+                </a> */}
             </div>
-            <div className={styles.img__container}>
-                <img className={styles.img__content} alt='dev' src={img}></img>
-            </div>
-            <div style={{display:'flex',justifyContent:'space-between',width:200,alignItems:'center',marginTop:8}}>
+            <div className={styles.social_media}>
                 <a href='https://www.instagram.com/i.joel_antony_joy' rel="noreferrer" target='_blank'><img alt='insta' src={insta}></img></a>
                 <a href='https://www.linkedin.com/in/joel-antony-joy' rel="noreferrer" target='_blank'><img alt='linkedin' src={linkedin}></img></a>
                 <a href='https://github.com/joelantonyjoy' rel="noreferrer" target='_blank'><img alt='github' src={github}></img></a>
